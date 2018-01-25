ccd990f63533d8d5eb45bff299f9dd1d0996bc13
rennes <rennes@openmailbox.org>
2018-01-24 23:17:13 -0600

    doc: Add operating system configuration template.

---
+++ doc/os-config-hurd.scm
@@ -0,0 +1,32 @@
+(use-modules (gnu))
+(use-package-modules hurd)
+
+(operating-system
+  (host-name "antelope")
+  (timezone "Europe/Paris")
+  (locale "en_US.utf8")
+  
+  ;; Assuming /dev/sdX is the target hard disk, and "my-root" is
+  ;; the label of the target root file system.
+  (bootloader (bootloader-configuration
+               (bootloader grub-bootloader)
+               (target "/dev/hd2s1")))
+  
+  (kernel gnumach)
+  
+  (file-systems (cons (file-system
+                        (device "my-root")
+                        (title 'label)
+                        (mount-point "/guix")
+                        (type "ext2"))
+                      %base-file-systems))
+  
+  (users (cons (user-account
+                (name "prometheus")
+                (comment "Welcome to the Hurd")
+                (group "users")
+                (home-directory "/home/prometheus"))
+               %base-user-accounts))
+
+  (packages (cons* 
+	     %base-packages-hurd)))
