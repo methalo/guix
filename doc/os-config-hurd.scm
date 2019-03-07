800a6223d1484adf1974affc35eea8fd42acd980
Rene <ikhuuma@gmail.com>
2019-03-07 12:00:10 -0800

    template: Add os-config-hurd.scm.

--- doc/os-config-hurd.scm
+++ doc/os-config-hurd.scm
@@ -1,33 +1,50 @@
+;; This is an operating system configuration template
+;; for a "bare bones" setup, with no X11 display server.
+
 (use-modules (gnu))
-(use-package-modules hurd)
+(use-service-modules networking ssh)
+(use-package-modules hurd screen ssh)
 
 (operating-system
-  (host-name "antelope")
-  (timezone "Europe/Paris")
+  (host-name "komputilo")
+  (timezone "Europe/Berlin")
   (locale "en_US.utf8")
-  
+
   ;; Assuming /dev/sdX is the target hard disk, and "my-root" is
   ;; the label of the target root file system.
   (bootloader (bootloader-configuration
-               (bootloader grub-bootloader)
-               (target "/dev/hd2s1")))
-  
-  (kernel gnumach)
-  
+                (bootloader grub-bootloader)
+                (target "/dev/hd2s1")))
   (file-systems (cons (file-system
                         (device "my-root")
                         (title 'label)
-                        (mount-point "/guix")
+                        (mount-point "/")
                         (type "ext2"))
                       %base-file-systems))
-  
+  (kernel gnumach)
+
+  (firmware '())
+
+;  (initrd '())
+
+  ;; This is where user accounts are specified.  The "root"
+  ;; account is implicit, and is initially created with the
+  ;; empty password.
   (users (cons (user-account
-                (name "prometheus")
-                (comment "Welcome to the Hurd")
+                (name "alice")
+                (comment "Bob's sister")
                 (group "users")
-                (home-directory "/home/prometheus"))
+
+                ;; Adding the account to the "wheel" group
+                ;; makes it a sudoer.  Adding it to "audio"
+                ;; and "video" allows the user to play sound
+                ;; and access the webcam.
+                (supplementary-groups '("wheel"
+                                        "audio" "video"))
+                (home-directory "/home/alice"))
                %base-user-accounts))
 
-  (packages (cons* 
+  (packages (cons*
 	     %base-packages-hurd))
-  (services %base-services-hurd))
+
+  (services %base-services-hurd)) ;;
